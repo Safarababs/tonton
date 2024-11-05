44fc9393299264af7cc975cfc88c68b9f76247c1
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2024-11-05 15:03:01 +0300

    blank

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import AboutUs from "./components/AboutUs";
-import Contact from "./components/contact";
-import Features from "./components/Features";
-import Footer from "./components/footer/Footer";
-import Popup from "./components/footer/Popup";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Pricing from "./components/pricing/Pricing";
-import Services from "./components/Services";
+// import AboutUs from "./components/AboutUs";
+// import Contact from "./components/contact";
+// import Features from "./components/Features";
+// import Footer from "./components/footer/Footer";
+// import Popup from "./components/footer/Popup";
+// import Header from "./components/Header";
+// import Home from "./components/Home";
+// import Pricing from "./components/pricing/Pricing";
+// import Services from "./components/Services";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     
-     <Home />
+      {/* <Header /> */}
+
+      {/* <Home />
      <Popup />
      <Features />
      <Services />
      <Pricing />
      <AboutUs />
-     <Contact />
+     <Contact /> */}
 
-     <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
